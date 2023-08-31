afba3962ed49ad8ff8a1b4c6809ff12001fd4b2d
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-08-31 16:04:01 +0100

    added example test file

---
+++ tests/examples/test_example.py
@@ -0,0 +1,20 @@
+from ..base import BasePlaywrightTestCase, expect
+
+from model_bakery import baker
+
+
+class TestCashflows(BasePlaywrightTestCase):
+    def setUp(self):
+        """
+        Add any setUp functions here
+        This will run beforeEach other method
+        eg baker.make()
+        """
+        self.page.goto(f"{self.live_server_url}/admin")
+
+    def test_method(self):
+        """
+        Main test assertions in here
+        eg expect()
+        """
+        expect(self.page.get_by_text("Django")).to_contain_text("Django")
